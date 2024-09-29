095be5ba37d075f6df15a5232da06a8cdce0d738
sahilav <sahilvernekar19@gmail.com>
2024-09-29 19:05:13 +0530

    Add files via upload

---
+++ weather/index.js
@@ -0,0 +1,28 @@
+var inputvalue = document.querySelector('#cityinput');
+var btn = document.querySelector('#add');
+var city = document.querySelector('#cityoutput');
+var description = document.querySelector('#description');
+var temp = document.querySelector('#temp');
+var wind = document.querySelector('#wind');
+var apik = "775ea7914cb92f8a5ac567165c796a8a";
+
+function convertion(val) {
+    return (val - 273).toFixed(2); // Fixed to two decimal places
+}
+
+btn.addEventListener('click', function() {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputvalue.value + '&appid=' + apik)
+        .then(res => res.json())
+        .then(data => {
+            var nameval = data['name'];
+            var descrip = data['weather'][0]['description'];
+            var tempature = data['main']['temp'];
+            var windspeed = data['wind']['speed'];
+
+            city.innerHTML = `Weather of <span>${nameval}</span>`;
+            temp.innerHTML = `Temperature: <span>${convertion(tempature)}°C</span>`;
+            description.innerHTML = `Sky Conditions: <span>${descrip}</span>`;
+            wind.innerHTML = `Wind Speed: <span>${windspeed} km/h</span>`;
+        })
+        
+});
